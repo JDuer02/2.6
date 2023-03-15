1b8f8d2f00684d1b84c178c5ce5c64c3595e6313
JDuer02 <126687677+JDuer02@users.noreply.github.com>
2023-03-15 14:28:34 +0000

    czesc1

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    float T=0;
+    float f=((9/5)*T+32);
+    while (T<=300)
+      {
+        System.out.println("T(celsius): " + T);
+      }
   }
 }
